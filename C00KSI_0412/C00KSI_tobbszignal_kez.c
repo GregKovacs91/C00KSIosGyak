7224bec1a87dde079c17b326a095d08919acdd10
GregKovacs91 <36516107+GregKovacs91@users.noreply.github.com>
2021-04-17 17:43:33 +0200

    Add files via upload

--- C00KSI_0412/C00KSI_tobbszignal_kez.c
+++ C00KSI_0412/C00KSI_tobbszignal_kez.c
@@ -1,18 +1,60 @@
-#include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
+void SzignalKez();
 
+int main(){
 
-main ()
-{
-int i;
-	unsigned sec=1;
+    signal(SIGINT, SzignalKez);
+    signal(SIGQUIT, SzignalKez);
 
-	signal(SIGINT, do_int);
+    for (int i = 0; i < 10; i++){
+        printf("Szignalra varok!\n");
+        pause();
+    }
 
-	signal(SIGALRM, do_nothing);
-	printf(" \n");
-	pause();
-    	printf("   \n");
-}	
-void do_nothing(){ ;}
+    return 0;
+}
+
+void SzignalKez(int sig){
+char tomb[60];
+
+switch(sig){
+    case 1: {
+        strcpy(tomb, "megallitas\n");
+        break;
+	}
+
+	case 2: {
+        strcpy(tomb, "kilepo szignal\n");
+        signal(SIGINT, SIG_DFL);
+        printf("\nKilepes: ctrl +c!\n");
+        break;
+	}
+
+	case 3: {
+        strcpy(tomb, "kilepes\n");
+        break;
+	}
+
+	case 4: {
+        strcpy(tomb, "nem megfelelo szignal\n");
+        break;
+	}
+
+	case 5: {
+        strcpy(tomb, "trap szignál\n");
+        break;
+	}
+
+	case 6: {
+        strcpy(tomb, "visszavono szignal\n");
+        break;
+	}
+
+        printf("\nA megfogott szignal neve: %s\n", tomb);
+        }
+}
